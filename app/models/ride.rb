9a9047cd2492daab82f14cfe49887f102944814c
John Tuttle <thejwt@gmail.com>
2017-12-04 00:53:28 +0000

    finished model tests

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,21 @@
 class Ride < ActiveRecord::Base
-  # write associations here
+  belongs_to :user
+  belongs_to :attraction
+
+  def take_ride
+    if self.user.tickets < self.attraction.tickets && self.user.height < self.attraction.min_height
+      "Sorry. You do not have enough tickets to ride the Roller Coaster. You are not tall enough to ride the Roller Coaster."
+    elsif self.user.tickets < self.attraction.tickets
+      "Sorry. You do not have enough tickets to ride the Roller Coaster."
+    elsif self.user.height < self.attraction.min_height
+      "Sorry. You are not tall enough to ride the Roller Coaster."
+    else
+      #binding.pry
+      new_tickets = self.user.tickets - self.attraction.tickets
+      new_happiness = self.user.happiness + self.attraction.happiness_rating
+      new_nausea = self.user.nausea + self.attraction.nausea_rating
+      self.user.update(tickets: new_tickets, happiness: new_happiness, nausea: new_nausea)
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+
+  has_secure_password
+
+  validates :password_digest, presence: true
+
+  def mood
+    if self.nausea > self.happiness
+      "sad"
+    else
+      "happy"
+    end
+  end
+
 end
